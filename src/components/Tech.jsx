62a8f260affd486d134bd39e613cb663100cdb2a
Stahlynx <168390257+TheoKurniady19623077@users.noreply.github.com>
2024-10-11 19:53:29 +0700

    feat: tech scale on hover

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,38 +6,38 @@ const Tech = () => {
   return (
     <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
       <h2 className="my-20 text-center text-4xl">Technology</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+      <div className="flex flex-wrap items-center justify-center gap-4 group scale-[0.85]">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <RiReactjsLine className="size-20 text-7xl text-cyan-400"/>
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4 ">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4 ">
           <RiHtml5Fill className="size-20 text-7xl text-orange-700"/>
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <RiCss3Fill className="size-20 text-7xl text-cyan-500"/>
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <RiTailwindCssFill className="size-20 text-7xl text-teal-600"/>
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <RiJavascriptLine className="size-20 text-7xl text-yellow-400"/>
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <RiNextjsLine className="size-20 text-7xl text-gray-600"/>
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <img className="text-7xl text-gray-600 size-20" src="/java.png" alt="" />
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <img className="text-7xl text-gray-600 size-20" src="/python.png" alt="" />
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <img className="text-7xl text-gray-600 size-20" src="/C.png" alt="" />
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <img className="text-7xl text-gray-600 size-20" src="/Cpp.png" alt="" />
         </div>
-        <div className="rounded-2xl border-4 border-purple-900 p-4">
+        <div className="rounded-2xl group  hover:scale-[1.25] transition-all duration-300 border-4 border-purple-900 p-4">
           <img className="text-7xl text-gray-600 size-20" src="/mysql.png" alt="" />
         </div>
       </div>
